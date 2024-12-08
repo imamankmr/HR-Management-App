2e44b546edf7ab110707bb393814eca403678136
Aman Kumar <110666477+imamankmr@users.noreply.github.com>
2024-12-09 02:34:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+    port: process.env.PORT || 3000, 
+  },
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     include: ["jwt-decode"]
 },
