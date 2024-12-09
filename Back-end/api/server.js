ae198c36d26eebe355b1fb95ff56788f1f2af218
imamankmr <amanjnvr@gmail.com>
2024-12-09 09:44:06 +0530

    fix-bugs  back-end

---
+++ Back-end/api/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const verifyToken = require('./middleware/authMiddleware');
+const bodyParser = require('body-parser');
+const candidatesRoutes = require("./routes/candidatesRoutes");
+const path = require("path");
+const employeeRoutes = require("./routes/employeeRoutes");
+
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ;
+
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/candidates", candidatesRoutes);
+app.use("/api/employees", employeeRoutes); 
+
+
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
