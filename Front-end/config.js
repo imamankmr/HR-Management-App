8019bb841f12eb747cd3354d034beb730840b8d4
Aman Kumar <110666477+imamankmr@users.noreply.github.com>
2024-12-18 23:15:11 +0530

    Update config.js

--- Front-end/config.js
+++ Front-end/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: "https://hr-management-app-bw1n.vercel.app",
+    API_BASE_URL: "https://hr-management-app-2-fo8m.onrender.com",
   };
   
   export default config;
